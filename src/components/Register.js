90711d11e858321a3e38b38e1cdb053f5bdc9aee
alejandropascuale <apascuale@outlook.com>
2021-11-01 10:20:22 -0300

    change de id inputs in register

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,28 +5,33 @@ import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import Header from './Header'
 
 const Register = () => {
+    
+    const userValidation = (e) => {
+        e.preventDefaul();
+    }
+    
     return (
         <>
             <Header />
             <main className='main-container'>
-                <form className='login-form'>
+                <form className='login-form' action='http://localhost:3001/register' method='POST'>
 
-                    <label htmlFor='id-employee'>Nº legajo</label>
-                    <input type='number' id='id-employee' name='id-employee' autoFocus/>
+                    <label htmlFor='idEmployee'>Nº legajo</label>
+                    <input type='number' id='idEmployee' name='idEmployee' autoFocus/>
                     
-                    <label htmlFor='first-name'>Nombre y Apellido</label>
-                    <input type='text' id='first-name' name='first-name' />
+                    <label htmlFor='names'>Nombre y Apellido</label>
+                    <input type='text' id='names' name='names' />
                     
-                    <label htmlFor='user-name'>Usuario</label>
-                    <input type='text' id='user-name' name='user-name' />
+                    <label htmlFor='userName'>Usuario</label>
+                    <input type='text' id='userName' name='userName' />
                     
                     <label htmlFor='password'>Contraseña</label>
-                    <input type='password' id='password' name='password' />
+                    <input type='password' id='password' name='password' autoComplete='true'/>
                     
-                    <label htmlFor='password'>Repite contraseña</label>
-                    <input type='password' id='password' name='password' />
+                    <label htmlFor='confirm-password'>Repite contraseña</label>
+                    <input type='password' id='confirm-password' name='confirm-password' autoComplete='true'/>
 
-                    <button type='submit' id='login-button'><FontAwesomeIcon icon={ faSignInAlt } /></button>
+                    <button onClick={userValidation} type='submit' id='register-button'><FontAwesomeIcon icon={ faSignInAlt } /></button>
                 </form>
             </main>
         </>
