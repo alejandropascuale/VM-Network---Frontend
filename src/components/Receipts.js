2e1ed32c535755b8dbf9e3fda386dbdf0dbfe9ef
alejandropascuale <apascuale@outlook.com>
2021-11-03 13:25:01 -0300

    update format to Receipts component

--- src/components/Receipts.js
+++ src/components/Receipts.js
@@ -10,7 +10,22 @@ const Receipts = () => {
             <br></br>
             <br></br>
             <br></br>
-            <Link to='/files/Copia de VM - Recibos de Sueldos 2021-10.pdf' target="_blank" download>Descargar recibo</Link>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Periodo</th>
+                            <th>Action</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Octubre 2021</td>
+                            <td><Link to='/files/Copia de VM - Recibos de Sueldos 2021-10.pdf' target="_blank" download>Descargar recibo</Link></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
